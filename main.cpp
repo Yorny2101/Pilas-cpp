07f222c452fe7d1ce7b6f3a56b525681ea325aee
Yorny <fundoraluzbetyorny@gmail.com>
2022-05-01 10:39:02 -0400

    funcion para agregar elementos a la pila

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+using namespace std;
+
+struct Nodo{   //nodo de la pila
+    int dato;
+    Nodo *siguiente;
+};
+
+void add_pile(Nodo *&, int);
 
 int main() {
+
+}
+
+void add_pile(Nodo *&pila, int n){
+    Nodo *nuevo_nodo = new Nodo; //asignacion dinamica de memoria para un nuevo nodo
+    nuevo_nodo ->dato = n;
+    nuevo_nodo -> siguiente = pila; //enlace con los datos q se tenian con anterioridad
+    pila = nuevo_nodo;              // asinacion del nuevo nodo a pila
+    
+
+
 
 }
